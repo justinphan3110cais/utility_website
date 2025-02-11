a05ac1f0564f90e99face9ac1ccf470f9534e35b
Bruce Lee <brucelws@seas.upenn.edu>
2025-02-11 09:50:21 -0500

    author name change

--- website/app/constants.tsx
+++ website/app/constants.tsx
@@ -2,6 +2,6 @@ export const PAPER_URL = "https://drive.google.com/file/d/1QAzSj24Fp0O6GfkskmnUL
 export const GITHUB_URL = "https://github.com/centerforaisafety/emergent-values";
 export const BIBTEX_CITATION = `@article{mazeika2025utility,
     title = {Utility Engineering: Analyzing and Controlling Emergent Value Systems in AIs},
-    author = {Mantas Mazeika and Xuwang Yin and Rishub Tamirisa and Jaehyuk Lim and Bruce W Lee and Richard Ren and Long Phan and Norman Mu and Adam Khoja and Oliver Zhang and Dan Hendrycks},
+    author = {Mantas Mazeika and Xuwang Yin and Rishub Tamirisa and Jaehyuk Lim and Bruce W. Lee and Richard Ren and Long Phan and Norman Mu and Adam Khoja and Oliver Zhang and Dan Hendrycks},
     year = {2025}
 }`;
