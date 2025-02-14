237be3ad8320467306aa95cee1b9b4629099a537
Long Phan <131222390+justinphan3110cais@users.noreply.github.com>
2025-02-14 09:14:31 -0800

    Update constants.tsx

--- website/app/constants.tsx
+++ website/app/constants.tsx
@@ -1,7 +1,11 @@
-export const PAPER_URL = "https://drive.google.com/file/d/1QAzSj24Fp0O6GfkskmnULmI1Hmx7k_EJ/view?usp=sharing";
+export const PAPER_URL = "https://arxiv.org/abs/2502.08640";
 export const GITHUB_URL = "https://github.com/centerforaisafety/emergent-values";
-export const BIBTEX_CITATION = `@article{mazeika2025utility,
-    title = {Utility Engineering: Analyzing and Controlling Emergent Value Systems in AIs},
-    author = {Mantas Mazeika and Xuwang Yin and Rishub Tamirisa and Jaehyuk Lim and Bruce W. Lee and Richard Ren and Long Phan and Norman Mu and Adam Khoja and Oliver Zhang and Dan Hendrycks},
-    year = {2025}
+export const BIBTEX_CITATION = `@misc{mazeika2025utilityengineeringanalyzingcontrolling,
+      title={Utility Engineering: Analyzing and Controlling Emergent Value Systems in AIs}, 
+      author={Mantas Mazeika and Xuwang Yin and Rishub Tamirisa and Jaehyuk Lim and Bruce W. Lee and Richard Ren and Long Phan and Norman Mu and Adam Khoja and Oliver Zhang and Dan Hendrycks},
+      year={2025},
+      eprint={2502.08640},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG},
+      url={https://arxiv.org/abs/2502.08640}, 
 }`;
